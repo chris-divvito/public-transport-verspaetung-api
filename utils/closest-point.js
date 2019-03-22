d6d9bbb15645cf3e53e6fc597b3a24f94e3c6b0d
chris <chris@divvito.com>
2019-03-23 09:59:09 +1000

    Clean up Closest Point a bit

--- utils/closest-point.js
+++ utils/closest-point.js
@@ -1,13 +1,12 @@
 const _ = require("lodash");
 
 const ClosestPoint = (stops, x, y) => {
-	let current_location = { x, y };
-	let locations = _.map(stops, ({ x, y }) => {
-		return { x, y };
-	});
-	function distance(p1, p2) {
-		var dx = Math.abs(p1.x - p2.x);
-		var dy = Math.abs(p1.y - p2.y);
+	const current_location = { x, y };
+	const locations = _.map(stops, ({ x, y }) => ({ x, y }));
+
+	function calculateDistance(p1, p2) {
+		const dx = Math.abs(p1.x - p2.x);
+		const dy = Math.abs(p1.y - p2.y);
 		return Math.sqrt(dx * dx + dy * dy);
 	}
 
@@ -15,19 +14,12 @@ const ClosestPoint = (stops, x, y) => {
 		if (arr.length < 2) {
 			return Infinity;
 		} else {
-			var minDist = distance(arr[0], current_location);
-			var minPoints = arr.slice(0, 1);
-
-			for (var j = 0; j < arr.length; j++) {
-				if (distance(current_location, arr[j]) < minDist) {
-					minDist = distance(current_location, arr[j]);
-					minPoints = [arr[j]];
-				}
-			}
-			return {
-				distance: minDist,
-				points: minPoints
-			};
+			return _.reduce(arr, (result, point) => {
+				const distance = calculateDistance(current_location, point);
+				return !result || distance < result.distance
+					? {distance, point}
+					: result;
+			}, null);
 		}
 	}
 
@@ -38,4 +30,5 @@ const ClosestPoint = (stops, x, y) => {
 
 	return { getClosestPoint };
 };
+
 module.exports = ClosestPoint;
